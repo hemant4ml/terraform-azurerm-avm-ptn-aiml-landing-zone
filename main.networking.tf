5c567085e52d25b62bebbaeef69d520518531763
Hemant Bhatt <hemant@ABCs-MacBook-Air.local>
2025-11-21 17:07:39 +0000

    fix firewall rt table

--- main.networking.tf
+++ main.networking.tf
@@ -82,14 +82,14 @@ module "firewall_route_table" {
   name                          = local.route_table_name
   resource_group_name           = azurerm_resource_group.this.name
   bgp_route_propagation_enabled = true
-  routes = {
+  routes = var.firewall_definition.deploy ? {
     azure_firewall = {
       name                   = "default-to-firewall"
       address_prefix         = "0.0.0.0/0"
       next_hop_type          = "VirtualAppliance"
       next_hop_in_ip_address = module.firewall[0].resource.ip_configuration[0].private_ip_address
     }
-  }
+  } : {}
 }
 
 module "fw_pip" {
